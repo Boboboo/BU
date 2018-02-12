cef52d913d2a2ab0ecb300cc59dd445ec78aa61f
Bonnie <boofcourse@gmail.com>
2018-02-12 16:26:37 -0500

    Update updateId.java

--- updateIDMethod/src/com/data/updateId.java
+++ updateIDMethod/src/com/data/updateId.java
@@ -36,41 +36,41 @@ public class updateId {
 	            "jdbc:postgresql://localhost:5432/postgres","postgres", "722722");
 	         
 	        
-	         Statement stmt = con.createStatement();
+	          Statement stmt = con.createStatement();
 	         ResultSet rs = stmt.executeQuery("SELECT * FROM links3");
 	         int id=0;
 	         while (rs.next()) {
-	        	id++;
-			pvalue=rs.getDouble("pvalue");            
+	        	id++;          
 			bio_entity_1=rs.getString("bio_entity_1"); 
 			entity_1_name=rs.getString("entity_1_name");     
 			bio_entity_2=rs.getString("bio_entity_2");   
-			entity_2_name=rs.getString("entity_2_name");     
-			weight=rs.getDouble("weight");             
+			entity_2_name=rs.getString("entity_2_name");              
 			entity_1_type=rs.getString("entity_1_type");     
 			entity_2_type=rs.getString("entity_2_type");     
 			contextid=rs.getString("contextid");        
 			interaction_type=rs.getString("interaction_type");  
 			habitat_1=rs.getString("habitat_1");        
 			habitat_2=rs.getString("habitat_2");
+			pvalue=rs.getDouble("pvalue");  
+			weight=rs.getDouble("weight");   
 
 			String sql = "INSERT INTO links VALUES (?, ?,?, ?,?, ?,?, ?,?, ?,?, ?,?)";
 
 			PreparedStatement pstmt = con.prepareStatement(sql);
 
 			pstmt.setInt(1, id);
-			pstmt.setDouble(2, pvalue);
-			pstmt.setString(3, bio_entity_1);
-			pstmt.setString(4, entity_1_name);
-			pstmt.setString(5, bio_entity_2);
-			pstmt.setString(6, entity_2_name);
-			pstmt.setDouble(7, weight);
-			pstmt.setString(8, entity_1_type);
-			pstmt.setString(9, entity_2_type);
-			pstmt.setString(10, contextid);
-			pstmt.setString(11, interaction_type);
-			pstmt.setString(12, habitat_1);
-			pstmt.setString(13, habitat_2);
+			pstmt.setString(2, bio_entity_1);
+			pstmt.setString(3, entity_1_name);
+			pstmt.setString(4, bio_entity_2);
+			pstmt.setString(5, entity_2_name);
+			pstmt.setString(6, entity_1_type);
+			pstmt.setString(7, entity_2_type);
+			pstmt.setString(8, contextid);
+			pstmt.setString(9, interaction_type);
+			pstmt.setString(10, habitat_1);
+			pstmt.setString(11, habitat_2);
+			pstmt.setDouble(12, pvalue);
+			pstmt.setDouble(13, weight);
 			pstmt.executeUpdate();
 	         }
 		     
