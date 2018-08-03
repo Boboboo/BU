169e3d4babf87c84206f27dd4eaac2d6964fddc9
Boboboo <boofcourse@gmail.com>
2018-08-03 00:45:45 -0400

    update original_nodes works

--- UpdateFromNCBI/src/visant/service/ProcessManager.java
+++ UpdateFromNCBI/src/visant/service/ProcessManager.java
@@ -23,12 +23,12 @@ public class ProcessManager {
 			readAllData(filePath);	
 		}
 		
-		System.out.println("Output report.txt file has been created successfully.");	
+		System.out.println("Update original_nodes successfully.");	
 	}
 	
 	
 	private String getFilePath() {
-		System.out.println("Please input the path of directory which contains data.txt. Example /Users/air/Desktop");
+		System.out.println("Please input the path of directory which contains nodes.dmp. Example /Users/air/Desktop");
 		
 		//get the input data.txt file path in the keyboard 
 		Scanner scanner =new Scanner(System.in);      
@@ -36,7 +36,6 @@ public class ProcessManager {
 		String filename="nodes.txt";
 		File file;
 		
-		//get the data.txt file path
 		while(true){
 			inputPath=scanner.nextLine();
 			if(inputPath.charAt(inputPath.length()-1)!='/') {
@@ -58,6 +57,10 @@ public class ProcessManager {
 	
 	private void readAllData(String path)  throws Exception {  
 		 System.out.println("readData path: "+path);
+		 
+		 DBConnection DBconn=new DBConnection();
+		 Connection conn=DBconn.getDBConnection();
+		 String sql="";
 	
 		 FileInputStream fstream = new FileInputStream(path);
 		 BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
@@ -65,68 +68,57 @@ public class ProcessManager {
 		 
 		 int taxid=0, parent_taxid=0;
 		 String rank=null;
-
+		 
 		 try {
+			 initailTable("original_nodes");
+			 
 			 while ((strLine = br.readLine()) != null)   {
 				 String[] array=strLine.split("\t\\|\t");
 				 taxid=Integer.valueOf(array[0].trim());
 				 parent_taxid=Integer.valueOf(array[1].trim());
 				 rank=array[2].trim();
 				 
-				 checkEachRow(taxid, parent_taxid,rank);
-
-	
-	      }
+				 insertEachRow(DBconn,conn,taxid, parent_taxid, rank);
+			 }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	    br.close(); 
 	}
 	
-	private void checkEachRow(int taxid,int parent_taxid,String rank) {
-		DBConnection DBconn=new DBConnection();
-		Connection conn=DBconn.getDBConnection();
-		Statement statement=null;
-		ResultSet results=null;
+	private void insertEachRow(DBConnection DBconn,Connection conn,int taxid,int parent_taxid,String rank) {
 		String sql="";
 		
-		try {
-			statement = conn.createStatement();
-			sql="SELECT * FROM original_nodes where taxid='"+taxid+"'" +
-		    		" AND"+
-		    		" rank='"+rank+"'";
-					        	    
-		     results = statement.executeQuery(sql);
-	  
-		     while(results.next()) {
-		    	 	Integer taxID = results.getInt("taxid");
-		   	    Integer parentID=results.getInt("parent_taxid");
-		       	String rankFromDB=results.getString("rank");  
-		       	
-		       	if(results==null) {
-		       		String sqlInsert = "INSERT INTO original_nodes VALUES (?, ?, ?)";
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					pstmt.setInt(1, taxid);
-		        	    pstmt.setInt(2, parent_taxid);
-		        	    pstmt.setString(3, rank);
-		          	pstmt.executeUpdate();
-		          	System.out.println(taxid+"*"+parent_taxid+"*"+rank);
-		       	}else {
-		       		if(parentID!=parent_taxid) {
-		       			statement = conn.createStatement();
-						String sqlUpdate = "UPDATE original_nodes SET parent_taxid='"+parent_taxid+
-								"' WHERE taxid='"+taxid+"'" +
-								" AND"+
-							    	" rank='"+rank+"'"; 
-						statement.executeUpdate(sql);
-						System.out.println(taxid+"---"+parent_taxid+"---"+rank);
-		       		}
-		       	}
-		    }
+		try {		
+			sql = "INSERT INTO original_nodes VALUES (?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, taxid);
+	        	pstmt.setInt(2, parent_taxid);
+	        	pstmt.setString(3, rank);
+	        pstmt.executeUpdate();    		
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	private void initailTable(String tableName) {	
+		DBConnection DBconn=new DBConnection();
+		Connection conn=DBconn.getDBConnection();
+		Statement statement=null;
+		String sql="";
+		
+		try {
+			statement = conn.createStatement();
+			sql = "DELETE FROM "+tableName; 
+			statement.executeUpdate(sql);
+			System.out.println(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("initailTable finished");
+	}
+	
 	
 	private  void writeUsingFileWriter(String data,String path) {
         File file = new File(path+"result.txt");
